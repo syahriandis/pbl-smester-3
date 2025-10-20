814c019e9151a7a8d2618a537a16e8d4890aac0d
dwikyputradachi <dwkyputra21@gmail.com>
2025-10-20 14:42:22 +0700

    Menghubungkan semua halaman ke main.dart

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'pages/login_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Hawaii Garden',
+      theme: ThemeData(
+        fontFamily: 'Roboto',
+        colorScheme: ColorScheme.fromSeed(seedColor: const Color(0xFF164E47)),
+        useMaterial3: true,
+      ),
+      home: const LoginPage(),
+    );
+  }
+}
